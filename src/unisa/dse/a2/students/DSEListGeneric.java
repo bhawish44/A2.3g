1a1dbd896383a784504836c78feeabe92861f442
bhawish44 <130421291+bhawish44@users.noreply.github.com>
2025-06-01 19:16:48 +0500

    "Step 2
    Corrected NodeGeneric to match DSEListGeneric compatibility abd completed the step2

--- src/unisa/dse/a2/students/DSEListGeneric.java
+++ src/unisa/dse/a2/students/DSEListGeneric.java
@@ -2,75 +2,190 @@ package unisa.dse.a2.students;
 
 import unisa.dse.a2.interfaces.ListGeneric;
 
-/**
- * @author simont
- *
- */
-public class DSEListGeneric implements ListGeneric {
-	
-	public NodeGeneric head;
-	private NodeGeneric tail;
+public class DSEListGeneric<T> implements ListGeneric<T> {
 
-	public DSEListGeneric() {
-		
-	}
-	public DSEListGeneric(NodeGeneric head_) {
-	}
-	
-	//Takes a list then adds each element into a new list
-	public DSEListGeneric(DSEList other) { // Copy constructor. 
-	}
+    public NodeGeneric<T> head;
+    private NodeGeneric<T> tail;
 
-	//remove and return the item at the parameter's index
-	public void remove(int index) {
+    public DSEListGeneric() {
+        head = null;
+        tail = null;
+    }
 
-	}
+    public DSEListGeneric(NodeGeneric<T> head_) {
+        this.head = head_;
+        this.tail = head_;
+        while (tail != null && tail.next != null) {
+            tail = tail.next;
+        }
+    }
 
-	//returns the index of the String parameter 
-	public int indexOf(String obj) {
-	}
-	
-	//returns item at parameter's index
-	public void get(int index) {
-	}
+    public DSEListGeneric(DSEListGeneric<T> other) {
+        if (other == null || other.head == null) {
+            this.head = null;
+            this.tail = null;
+        } else {
+            this.head = new NodeGeneric<>(null, null, other.head.getItem());
+            NodeGeneric<T> currentOther = other.head.next;
+            NodeGeneric<T> currentThis = this.head;
 
-	//checks if there is a list
-	public boolean isEmpty() {
-	}
+            while (currentOther != null) {
+                NodeGeneric<T> newNode = new NodeGeneric<>(null, currentThis, currentOther.getItem());
+                currentThis.next = newNode;
+                currentThis = newNode;
+                currentOther = currentOther.next;
+            }
+            this.tail = currentThis;
+        }
+    }
 
-	//return the size of the list
-	public int size() {
-	}
-	
-	//Take each element of the list a writes them to a string 
-	@Override
-	public String toString() {
-	}
+    public boolean add(T obj) {
+        if (obj == null) return false;
+        NodeGeneric<T> newNode = new NodeGeneric<>(null, tail, obj);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+        return true;
+    }
 
-	//add the parameter item at of the end of the list
-	public boolean add(Object obj) {
-	}
+    public boolean isEmpty() {
+        return head == null;
+    }
 
-	//add item at parameter's index
-	public boolean add(int index, Object obj) {
-	}
+    public int size() {
+        int count = 0;
+        NodeGeneric<T> current = head;
+        while (current != null) {
+            count++;
+            current = current.next;
+        }
+        return count;
+    }
 
-	//searches list for parameter's String return true if found
-	public boolean contains(Object obj) {
-	}
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        NodeGeneric<T> current = head;
+        while (current != null) {
+            sb.append(current.getItem());
+            if (current.next != null) sb.append(" ");
+            current = current.next;
+        }
+        return sb.toString();
+    }
 
-	//removes the parameter's item form the list
-	public boolean remove(Object obj) {
-	}
-	
-	@Override
-	public int hashCode() {
-		return 0;
-	}
+    public boolean equals(Object other) {
+        if (this == other) return true;
+        if (!(other instanceof DSEListGeneric<?>)) return false;
+        DSEListGeneric<?> that = (DSEListGeneric<?>) other;
+        NodeGeneric<T> currentThis = this.head;
+        NodeGeneric<?> currentThat = that.head;
+        while (currentThis != null && currentThat != null) {
+            if (!currentThis.getItem().equals(currentThat.getItem())) {
+                return false;
+            }
+            currentThis = currentThis.next;
+            currentThat = currentThat.next;
+        }
+        return currentThis == null && currentThat == null;
+    }
 
-	@Override
-	public boolean equals(Object other) {
-		return true;
-	}
-	
+    public int indexOf(T obj) {
+        int index = 0;
+        NodeGeneric<T> current = head;
+        while (current != null) {
+            if (current.getItem().equals(obj)) return index;
+            current = current.next;
+            index++;
+        }
+        return -1;
+    }
+
+    public T get(int index) {
+        if (index < 0) return null;
+        NodeGeneric<T> current = head;
+        int count = 0;
+        while (current != null) {
+            if (count == index) return current.getItem();
+            current = current.next;
+            count++;
+        }
+        return null;
+    }
+
+    public boolean add(int index, T obj) {
+        if (obj == null || index < 0) return false;
+        if (index == 0) {
+            NodeGeneric<T> newNode = new NodeGeneric<>(head, null, obj);
+            if (head != null) head.prev = newNode;
+            head = newNode;
+            if (tail == null) tail = newNode;
+            return true;
+        }
+        NodeGeneric<T> current = head;
+        int count = 0;
+        while (current != null && count < index) {
+            current = current.next;
+            count++;
+        }
+        if (current == null) {
+            return add(obj);
+        } else {
+            NodeGeneric<T> newNode = new NodeGeneric<>(current, current.prev, obj);
+            if (current.prev != null) current.prev.next = newNode;
+            current.prev = newNode;
+            return true;
+        }
+    }
+
+    public boolean contains(T obj) {
+        return indexOf(obj) != -1;
+    }
+
+    public boolean remove(T obj) {
+        NodeGeneric<T> current = head;
+        while (current != null) {
+            if (current.getItem().equals(obj)) {
+                if (current.prev != null) current.prev.next = current.next;
+                else head = current.next;
+
+                if (current.next != null) current.next.prev = current.prev;
+                else tail = current.prev;
+
+                return true;
+            }
+            current = current.next;
+        }
+        return false;
+    }
+
+    public T remove(int index) {
+        if (index < 0) return null;
+        NodeGeneric<T> current = head;
+        int count = 0;
+        while (current != null) {
+            if (count == index) {
+                T data = current.getItem();
+                if (current.prev != null) current.prev.next = current.next;
+                else head = current.next;
+
+                if (current.next != null) current.next.prev = current.prev;
+                else tail = current.prev;
+
+                return data;
+            }
+            current = current.next;
+            count++;
+        }
+        return null;
+    }
+
+    @Override
+    public int hashCode() {
+        return toString().hashCode();
+    }
 }
--- src/unisa/dse/a2/students/NodeGeneric.java
+++ src/unisa/dse/a2/students/NodeGeneric.java
@@ -1,49 +1,42 @@
 package unisa.dse.a2.students;
 
 /**
- * ===========================================================
- * 
- * You shouldn't need to make any changes in this class
- * 
- * We'll be using our own version for marking
- * 
- * ===========================================================
- * 
+ * Corrected NodeGeneric class matching assignment requirements
  */
 public class NodeGeneric<T> {
 
-	public NodeGeneric<T> next;
-	public NodeGeneric<T> prev;
-	
-	private T t;
+    public NodeGeneric<T> next;
+    public NodeGeneric<T> prev;
 
-	public NodeGeneric(NodeGeneric<T> next, NodeGeneric<T> prev, T node) {
-		this.next = next;
-		this.prev = prev;
-		this.t = node;
-	}
+    private T t;
 
-	public T get() {
-		return t;
-	}
+    public NodeGeneric(NodeGeneric<T> next, NodeGeneric<T> prev, T node) {
+        this.next = next;
+        this.prev = prev;
+        this.t = node;
+    }
 
-	@Override
-	public boolean equals(Object other) {
-		if (this == other)
-			return true;
-		if (other == null)
-			return false;
-		if (!(other instanceof NodeGeneric<?>))
-			return false;
+    // Required method for item access in DSEListGeneric
+    public T getItem() {
+        return t;
+    }
 
-		return t.equals(((NodeGeneric<T>)other).get());
-	}
+    @Override
+    public boolean equals(Object other) {
+        if (this == other)
+            return true;
+        if (other == null)
+            return false;
+        if (!(other instanceof NodeGeneric<?>))
+            return false;
 
-	@Override
-	public int hashCode() {
-		if ( t == null )
-			return 0;
-		return t.hashCode();
-	}
+        return t.equals(((NodeGeneric<?>) other).getItem());
+    }
 
+    @Override
+    public int hashCode() {
+        if (t == null)
+            return 0;
+        return t.hashCode();
+    }
 }
