113dd6d888fee727e81f83b61af76d2edefb63e8
bhawish44 <130421291+bhawish44@users.noreply.github.com>
2025-06-01 19:23:09 +0500

    update trade.java

--- src/unisa/dse/a2/students/Trade.java
+++ src/unisa/dse/a2/students/Trade.java
@@ -1,112 +1,112 @@
 package unisa.dse.a2.students;
 
+/**
+ * Represents a trade order in the system.
+ * Each trade is associated with a broker, company code, and share quantity.
+ */
 public class Trade implements Comparable<Trade> {
 
-	/*
-	 * Don't modify the tradeID or created values, as they're used to simplify the tests
-	 * You may be required to use the "created" value in some parts of your code.
-	 */
-	private long tradeId = -1;
-	private long created;
+    /* Don't modify the tradeId or created values, as they're used to simplify the tests */
+    private long tradeId = -1;
+    private long created;
 
-	/**
-	 * @return Track the moment in time this Trade was created
-	 */
-	public void getCreated()
-	{
-	}
-	
-	public String listedCompanyCode;
+    private String listedCompanyCode;
+    private int shareQuantity;
+    private StockBroker broker;
 
-	/**
-	 * @return The company's code
-	 */
-	public void getCompanyCode() {
-	}
-	
-	private int shareQuantity;
+    /**
+     * @return The timestamp of trade creation
+     */
+    public long getCreated() {
+        return created;
+    }
 
-	/**
-	 * @return The quantity of shares to trade
-	 */
-	public void getShareQuantity() {
-	}
+    /**
+     * @return The company's stock code
+     */
+    public String getCompanyCode() {
+        return listedCompanyCode;
+    }
 
-	private StockBroker broker;
+    /**
+     * @return The quantity of shares in the trade
+     */
+    public int getShareQuantity() {
+        return shareQuantity;
+    }
 
-	/**
-	 * @return The broker associated with this trade
-	 */
-	public void getStockBroker() {
-	}
+    /**
+     * @return The broker processing this trade
+     */
+    public StockBroker getStockBroker() {
+        return broker;
+    }
 
+    /***
+     * Do not modify this constructor, it is used for testing purposes only
+     */
+    public Trade(StockBroker broker, int id) {
+        created = System.nanoTime();
+        tradeId = id;
+        try { Thread.sleep(100); } catch (Exception x) {}
+    }
 
-	/***
-	 * Do not modify this constructor, it is used for testing purposes only
-	 */
-	public Trade(StockBroker broker, int id)
-	{
-		created = System.nanoTime(); //do not change this
-		tradeId = id; //do not change this
-		try { Thread.sleep(100); } catch (Exception x) {}
-	}
-	
-	/***
-	 * Create a new trade with the associated broker, company, and share quantity
-	 * DO NOT change the current created and tradeId code
-	 * 
-	 * @param broker
-	 * @param listedCompanyCode
-	 * @param shareQuantity
-	 */
-	public Trade(StockBroker broker, String listedCompanyCode, int shareQuantity)
-	{
-		created = System.nanoTime(); //do not change this
-		tradeId = System.nanoTime(); //do not change this
-		try { Thread.sleep(100); } catch (Exception x) {}
-	}
-	
-	/**
-	 * Compares one trade to another trade
-	 * 
-	 * If we have two trades, A and B, and we examine the company in each trade:
-	 *  - if A and B are BOTH on their broker's watchlists, they are equal (return 0)
-	 *  - if A is on their brokers list, but B is not on B's brokers list (return 1)
-	 *  - if B is on their brokers list, but A is not on A's brokers list (return -1)
-	 *  - Otherwise, if neither trade is on their broker's list, then compare 
-	 *  		the "created" field, returning -1 if "this" is smaller, 0 if equal, 
-	 *  		or 1 if greater
-	 *  
-	 * @return The ordering priority of the trade
-	 */
-	public int compareTo(Trade t)
-	{
-	}
-	
+    /**
+     * Constructs a new Trade object with broker, company code, and share quantity
+     */
+    public Trade(StockBroker broker, String listedCompanyCode, int shareQuantity) {
+        created = System.nanoTime();
+        tradeId = System.nanoTime();
+        try { Thread.sleep(100); } catch (Exception x) {}
 
-	/***
-	 * Do not modify this toString, it is used for testing purposes
-	 */
-	@Override
-	public String toString() {
-		return ""+tradeId;
-	}
+        this.broker = broker;
+        this.listedCompanyCode = listedCompanyCode;
+        this.shareQuantity = shareQuantity;
+    }
 
-	/***
-	 * Do not modify this equals, it is used for testing purposes
-	 */
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Trade other = (Trade) obj;
-		if (tradeId != other.tradeId)
-			return false;
-		return true;
-	}
-	
+    /**
+     * Compares this trade to another trade.
+     * Priority logic:
+     * - If both trades are on their brokers' watchlists → 0
+     * - If only this trade is on its broker's watchlist → 1
+     * - If only the other trade is on its broker's watchlist → -1
+     * - Else compare by creation time
+     */
+    public int compareTo(Trade t) {
+        boolean thisOnWatchlist = broker.getWatchlist().contains(this.listedCompanyCode);
+        boolean otherOnWatchlist = t.broker.getWatchlist().contains(t.listedCompanyCode);
+
+        if (thisOnWatchlist && otherOnWatchlist) {
+            return 0;
+        } else if (thisOnWatchlist) {
+            return 1;
+        } else if (otherOnWatchlist) {
+            return -1;
+        } else {
+            return Long.compare(this.created, t.created);
+        }
+    }
+
+    /***
+     * Do not modify this toString, it is used for testing purposes
+     */
+    @Override
+    public String toString() {
+        return "" + tradeId;
+    }
+
+    /***
+     * Do not modify this equals, it is used for testing purposes
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Trade other = (Trade) obj;
+        return tradeId == other.tradeId;
+    }
 }
